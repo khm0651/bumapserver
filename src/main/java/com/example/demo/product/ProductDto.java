14cf869ca150f77ec287c3428853f620c326dcfb
hamin <khm0651@naver.com>
2021-08-07 17:58:25 +0900

    prmodify oduct api 수정

--- src/main/java/com/example/demo/product/ProductDto.java
+++ src/main/java/com/example/demo/product/ProductDto.java
@@ -4,22 +4,24 @@ public class ProductDto {
     public String uid;
     public String shop_uid;
     public String product_name;
+    public String product_img;
     public String product_cotegory;
     public int product_price;
-    public int like;
-    public String size;
-    public String color;
-    public String link;
+    public int proudct_like;
+    public String product_size;
+    public String product_color;
+    public String product_link;
 
-    public ProductDto(String uid, String shop_uid, String product_name, String product_cotegory, int product_price, int like, String size, String color, String link) {
+    public ProductDto(String uid, String shop_uid, String product_name, String product_img, String product_cotegory, int product_price, int proudct_like, String product_size, String product_color, String product_link) {
         this.uid = uid;
         this.shop_uid = shop_uid;
         this.product_name = product_name;
+        this.product_img = product_img;
         this.product_cotegory = product_cotegory;
         this.product_price = product_price;
-        this.like = like;
-        this.size = size;
-        this.color = color;
-        this.link = link;
+        this.proudct_like = proudct_like;
+        this.product_size = product_size;
+        this.product_color = product_color;
+        this.product_link = product_link;
     }
 }
