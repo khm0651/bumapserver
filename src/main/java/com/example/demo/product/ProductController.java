e4df1ff42a9afe058669279f13b86d4c7ac41435
hamin <khm0651@naver.com>
2021-08-19 20:37:31 +0900

    Add best api

--- src/main/java/com/example/demo/product/ProductController.java
+++ src/main/java/com/example/demo/product/ProductController.java
@@ -49,19 +49,37 @@ public class ProductController {
     @GetMapping("/product/category")
     @ResponseBody
     List<ProductDto> getProduct(@RequestParam("type") String type){
-        String query = "select " +
-                "p.uid, " +
-                "p.shop_uid, " +
-                "p.product_name, " +
-                "p.product_img, " +
-                "p.product_category, " +
-                "p.product_price, " +
-                "p.product_like, " +
-                "p.product_size, " +
-                "p.product_color, " +
-                "p.product_link, " +
-                "s.shop_name " +
-                "from product as p join shop as s on p.shop_uid = s.uid where p.product_category = '"+ type + "'";
+        String query = "";
+        if(type.equals("best")){
+            query = "select " +
+                    "p.uid, " +
+                    "p.shop_uid, " +
+                    "p.product_name, " +
+                    "p.product_img, " +
+                    "p.product_category, " +
+                    "p.product_price, " +
+                    "p.product_like, " +
+                    "p.product_size, " +
+                    "p.product_color, " +
+                    "p.product_link, " +
+                    "s.shop_name " +
+                    "from product as p join shop as s on p.shop_uid = s.uid order by p.product_like desc limit 9";
+        }else{
+            query = "select " +
+                    "p.uid, " +
+                    "p.shop_uid, " +
+                    "p.product_name, " +
+                    "p.product_img, " +
+                    "p.product_category, " +
+                    "p.product_price, " +
+                    "p.product_like, " +
+                    "p.product_size, " +
+                    "p.product_color, " +
+                    "p.product_link, " +
+                    "s.shop_name " +
+                    "from product as p join shop as s on p.shop_uid = s.uid where p.product_category = '"+ type + "'";
+        }
+
         List<Map<String, Object>> result = jdbcTemplate.queryForList(query);
         String content = Json.getJsonFrom(result);
         return Json.deserializeAsListOf(content, ProductDto.class);
